39c05320bcb96f060e889a3d2cbb170b8020de3c
walarcont89 <waalarcontobasura@mail.austral.edu.ar>
2024-04-09 22:01:01 +0000

    auto commit 1       20240409 220059

---
+++ src/ambiente/z301_GrabarMisSemillas-copy.r
@@ -0,0 +1,14 @@
+# Este script almacena definitivamente sus cinco semillas
+# en el bucket, de forma que NO deba cargarlas en cada script
+
+require( "data.table" )
+
+# reemplazar aqui por SUS semillas 
+mis_semillas <- c(299909, 299977, 300007, 300017, 300089)
+
+tabla_semillas <- as.data.table(list( semilla = mis_semillas ))
+
+fwrite( tabla_semillas,
+    file = "~/buckets/b1/datasets/mis_semillas.txt",
+    sep = "\t"
+)
